20a4d690e9c3bce22922e6ffffaaae348942d830
Michał Łuszczyk <michal.luszczyk@gmail.com>
2014-05-10 19:12:15 +0200

    set qt application name in main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 int main(int argc, char **argv)
 {
 	QApplication app(argc, argv);
+	app.setOrganizationName("LinOxide");
+	app.setOrganizationDomain("linoxide.com");
+	app.setApplicationName("Speedtest");
 
 	MainWindow mw;
 	mw.show();
